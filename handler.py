866334f864cb7966905735fc92617466d2daaef6
Almeida de Almeida <39647781+AlmeidaJoao@users.noreply.github.com>
2023-07-02 09:59:25 +0200

    update: dynamically reference to ARNs

--- handler.py
+++ handler.py
@@ -7,15 +7,7 @@ from datetime import datetime
 
 client = boto3.client("events")
 
-print('Loading function')
-
 def notifier(event, context):
-    # #print("Received event: " + json.dumps(event, indent=2))
-    # print("value1 = " + event['key1'])
-    # print("value2 = " + event['key2'])
-    # print("value3 = " + event['key3'])
-    # return event['key1']  # Echo back the first key value
-    # #raise Exception('Something went wrong')
     city = "Inhambane"
     params = {
         "q": city,
@@ -25,7 +17,7 @@ def notifier(event, context):
     geo_data = requests.get(url=os.getenv("GEO_API_URL"), params=params).json()
     lat = geo_data[0]["lat"]
     lon = geo_data[0]["lon"]
-    
+
     params = {
         "lat": lat,
         "lon": lon,
@@ -33,7 +25,7 @@ def notifier(event, context):
     }
     weather_data = requests.get(url=os.getenv("OPEN_WEATHER_API_URL"), params=params).json()
     current_wind_speed = weather_data["wind"]["speed"]
-    
+
     next_five_days_wind = []
     params = {
         "lat": lat,
@@ -41,38 +33,42 @@ def notifier(event, context):
         "appid": os.getenv("OPEN_WEATHER_API_KEY")
     }
     # This API provides the 5 five days weather in 3 hours steps
-    # It was the preferres option as it is free
+    # It was the preferred option as it is free :)
     next_days_weather = requests.get(url=os.getenv("OPEN_WEATHER_API_DAILY_URL"), params=params).json()["list"]
-    for i in  range(0, len(next_days_weather), 8):
+    for i in range(0, len(next_days_weather), 8):
         next_five_days_wind.append(next_days_weather[i]["wind"]["speed"])
-        
-    
+
     next_five_days_wind.insert(0, current_wind_speed)
     responses = []
-    sent = False
     # TODO: Create alerts types in a different file
-    # TODO: Notify for today, tomorrow, after tommorrow 
+    # TODO: Notify for today, tomorrow, after tomorrow
+
+    event_buses = client.list_event_buses(
+        NamePrefix="NotifierEvent",
+        Limit=1
+    )
+    event_bus_name = event_buses["EventBuses"][0]["Arn"]
+
     for wind in next_five_days_wind:
         if wind > 2:
+            # TODO: Adjust to send events on match with the alert types
             # Send data to event bus
             data = {
                 "city": city,
                 "wind_speed": wind
             }
-            # service_name = event["NotifierEvent"]
-            responses.append( client.put_events(
-                    Entries= [
-                        {
-                            "Time": datetime.now(),
-                            "Source": "com.almeidadealmeida.godseye", 
-                            "Resources": [],
-                            "DetailType": "weather", 
-                            "Detail": json.dumps(data),
-                            "EventBusName": "arn:aws:events:us-east-1:967906495397:event-bus/NotifierEvent"
-                            # "EventBusName": service_name
-                        }
-                    ]
-                )
+            responses.append(client.put_events(
+                Entries=[
+                    {
+                        "Time": datetime.now(),
+                        "Source": "com.almeidadealmeida.godseye",
+                        "Resources": [],
+                        "DetailType": "weather",
+                        "Detail": json.dumps(data),
+                        "EventBusName": event_bus_name
+                    }
+                ]
+            )
             )
             break
     return responses
